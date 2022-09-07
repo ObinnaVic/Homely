744a029e64f02b1513533d5b43bc11f69fd56faa
Obinna Victor <nkirevictor77@gmail.com>
2022-09-07 20:42:10 +0100

    some cart features updated

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,20 +7,23 @@ const Product = ({img, name, amount, price, increase, goToCart, openCart}) => {
     return (
       <>
         <div className="col-lg-4 col-md-6 col-sm-12 p-3 d-flex justify-content-center">
-          <div className="products d-flex flex-column align-items-center p-3">
+          <div className="products d-flex flex-column align-items-center p-4">
             <img src={img} className="food-img" alt="food" />
             <h6>{name}</h6>
-            <div className="d-flex align-items-center">
+            <div className="d-flex align-items-center price-sec">
               <h6 className="fw-bold pe-2 m-0">${price}</h6>
               <span>{amount}</span>
               <button
-                className="primary-color add-btn btn d-flex align-items-center text-light fw-bold"
+                className="primary-color add-btn btn d-flex align-items-center text-light m-2"
                 onClick={() => increase()}
               >
-                + ADD
+                <span className="fw-bold fs-3">+</span>
               </button>
             </div>
-            <button className="border-0 btn-link btn" onClick={() => openCart()}>
+            <button
+              className="border-0 btn-link btn"
+              onClick={() => openCart()}
+            >
               <h6 className={goToCart ? "text-color" : "d-none"}>Go to Cart</h6>
             </button>
           </div>
