0b4fb5a93122896541ca5abf4e78237cf029186d
Obinna Victor <nkirevictor77@gmail.com>
2022-09-08 22:15:23 +0100

    form bug fix

--- src/components/Products.js
+++ src/components/Products.js
@@ -10,18 +10,25 @@ const Products = ({data, modal, openModal, closeModal, getTotals}) => {
      getTotals();
    });
 
-    return (
+   const order = () => {
+    if (input) {
+      setInput("");
+      openModal();
+    } else {
+      return;
+    }
+   }
 
-      
+    return (
       // <div>FOOD DATA SECTION</div>
-      <div className='mt-4'>
+      <div className="mt-4">
         <div className="d-flex flex-column align-items-center">
           <h2 className="text-color">Our daily dishes</h2>
           <p>Check out recomended dishes of your choice</p>
         </div>
         <div className="row food-row">
           {data.map((item) => {
-            return <Product key={item.id} {...item}/>
+            return <Product key={item.id} {...item} />;
           })}
         </div>
         <div className="row food-row">
@@ -58,23 +65,21 @@ const Products = ({data, modal, openModal, closeModal, getTotals}) => {
               <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z" />
             </svg>
           </div>
-          <textarea
-            placeholder="Enlighten us"
-            cols="50"
-            rows="5"
-            style={{ width: "50%" }}
+          <input
+            type="text"
+            value={input}
+            className='input'
             onChange={(e) => setInput(e.target.value)}
-          ></textarea>
+          />
           <button
             className="btn primary-color mb-5 mt-3"
-            onClick={() => (input ? openModal() : "")}
+            onClick={() => order()}
           >
             Submit
           </button>
         </div>
 
-
-          {/* <div> MODAL </div> */}
+        {/* <div> MODAL </div> */}
         <div
           className={
             modal
